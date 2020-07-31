7db125358d7f1ddd4c9b16cbe497cb5f9ed21cd8
smiling-modem-1057 <nicole.v.meighan@gmail.com>
2020-07-31 18:27:22 +0000

    Automatically backed up by Learn

--- spec/rescue_princess_peach_spec.rb
+++ spec/rescue_princess_peach_spec.rb
@@ -1,10 +1,7 @@
 require "spec_helper"
 describe "#rescue_princess_peach" do
-  # rescue_princess_peach
-  # puts status
   it "outputs a message and sets a variable, status, that is not available outside of this method" do
     expect{rescue_princess_peach}.to output("Hooray! Mario has rescued Princess Peach.\n").to_stdout 
     expect{puts status}.to raise_error(NameError)
   end 
-  
-  # rescue_princess_peach
+  end
