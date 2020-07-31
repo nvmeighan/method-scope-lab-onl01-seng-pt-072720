32c9e4dad9decfff8454965062455e45b33dca45
smiling-modem-1057 <nicole.v.meighan@gmail.com>
2020-07-31 17:54:37 +0000

    Automatically backed up by Learn

--- spec/rescue_princess_peach_spec.rb
+++ spec/rescue_princess_peach_spec.rb
@@ -6,3 +6,5 @@ describe "#rescue_princess_peach" do
     expect{rescue_princess_peach}.to output("Hooray! Mario has rescued Princess Peach.\n").to_stdout 
     expect{puts status}.to raise_error(NameError)
   end 
+  
+  # rescue_princess_peach
